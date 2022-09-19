39baeb6369b0f35a735b39f06dc1908063b1f496
OhNoZero <cinkmarta@seznam.cz>
2022-09-19 14:41:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 10;
+        int num2 = 15;
+
+        int num3 = num + num2;
+        if (num == num2){
+
+        } else if (num > num2) {
+            
+        }
+    }
+}
